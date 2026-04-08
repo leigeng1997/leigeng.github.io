49607dcdc43967efc0d4df516e9e8163a4bdf220
leigeng1997 <lei.geng1997@gmail.com>
2026-04-08 21:59:42 +0000

    Deploying to gh-pages from @ leigeng1997/leigeng1997.github.io@5b47d46c4fc5cc66a6f7d38098125807194942ba 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "leigeng1997/leigeng.github.io",
+    "data-repo": "leigeng1997/leigeng1997.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
